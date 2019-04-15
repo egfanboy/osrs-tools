872c2cabd6f623102216a23ba51dab99b701bc7a
Eric Turriff <eric.turriff@gmail.com>
2019-04-14 21:20:02 -0400

    Fixed exp curve (#9)

--- src/utils/runescape/get-level-by-exp.js
+++ src/utils/runescape/get-level-by-exp.js
@@ -1,32 +1,30 @@
-const expForLevel = level => {
-    return Math.floor(
-        Math.floor(level + 300 * Math.pow(2, level / 7)) / 4
-    );
+/**
+ * Formula floor((SUM (1 to level - 1) -> floor(x + 300 *2^(x/7))/4)
+ * credit to http://rsdo.net/RSDOnline/Guides/guide.aspx?file=Experience%20formula.html
+ */
+
+const _expForLevels = [];
+
+const calculateExpForLevels = () => {
+    let exp = 0;
+    const maxLevel = 200;
+    for (let level = 1; level <= maxLevel; level++) {
+        exp = exp + Math.floor(level + 300 * 2 ** (level / 7));
+
+        _expForLevels.push(Math.floor(exp / 4));
+    }
 };
-const expForLevels = Array.from({ length: 126 }).reduce(
-    (acc, _, i) => {
-        const expForNextlevel = expForLevel(i + 1);
-
-        if (i === 0)
-            return (acc = [...acc, expForNextlevel]);
-
-        return (acc = [
-            ...acc,
-            acc[i - 1] + expForNextlevel,
-        ]);
-    },
-    []
-);
 
 const getLevelByExp = (exp = 0) => {
+    if (!_expForLevels.length) calculateExpForLevels();
+
+    // Case when exp is EXACTLY what you need for a level
+    if (_expForLevels.includes(exp)) return _expForLevels.findIndex(xp => xp === exp) + 2;
     // The +2 offsets the 0 based index and that you start at level 1
 
     return (
-        expForLevels.findIndex((_, index, list) => {
-            return (
-                exp >= list[index - 1] &&
-                exp <= list[index + 1]
-            );
+        _expForLevels.findIndex((_, index, list) => {
+            return exp >= list[index - 1] && exp <= list[index + 1];
         }) + 2
     );
 };
