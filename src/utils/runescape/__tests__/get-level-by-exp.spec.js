50ba8fa98b58f8be8ed16a9226d78124d5773f3b
ericTurf <eric.turriff@gmail.com>
2019-04-14 20:59:28 -0400

    Fixed exp curve

--- src/utils/runescape/__tests__/get-level-by-exp.spec.js
+++ src/utils/runescape/__tests__/get-level-by-exp.spec.js
@@ -1,4 +1,5 @@
 import getLevelByExp from '../get-level-by-exp';
+
 describe('getLevelByExp', () => {
     it('should be able to properly determine a level by exp amount', () => {
         const level1Exp = 70;
@@ -6,7 +7,9 @@ describe('getLevelByExp', () => {
         const level99Exp = 13034431;
 
         expect(getLevelByExp(level1Exp)).toEqual(1);
+        expect(getLevelByExp(level50Exp - 1)).toEqual(49);
         expect(getLevelByExp(level50Exp)).toEqual(50);
+        expect(getLevelByExp(level99Exp - 1)).toEqual(98);
         expect(getLevelByExp(level99Exp)).toEqual(99);
     });
 });
