43cabc2c6b7718de89484bfcccf7ed85910f1629
Eric Turriff <eric.turriff@gmail.com>
2019-04-16 21:50:29 -0400

    Ejected and added bundling for electron-main process (#15)
    * Ejected and added bundling for electron-main process

---
+++ config/webpack.main.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+module.exports = {
+    entry: path.join(__dirname, '..', 'main', 'index.js'),
+    output: {
+        path: path.resolve(__dirname, '..', 'public'),
+        filename: 'electron.js',
+    },
+    target: 'electron-main',
+    node: {
+        __dirname: false,
+    },
+};
